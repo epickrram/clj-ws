6fd95fa7b3ccecef1998229414b5289b2f4fdd16
epickrram <mark@epickrram.com>
2015-07-27 19:36:13 +0100

    delete unused code/tests

--- src/clj_ws/json.clj
+++ src/clj_ws/json.clj
@@ -2,11 +2,6 @@
   ^{:author mark}
   clj-ws.json)
 
-(def init "init")
-(def object-start "object-start")
-(def key-start "key-start")
-(def value-start "value-start")
-(def array-start "array-start")
 (def object-start-token-char (Character/valueOf (.charAt "{" 0)))
 (def object-end-token-char (Character/valueOf (.charAt "}" 0)))
 (def array-start-token-char (Character/valueOf (.charAt "[" 0)))
@@ -20,26 +15,12 @@
 (def whitespace-pattern (re-pattern "[\\s]+"))
 (def string-pattern (re-pattern "^\"[^\"]+\"$"))
 
-; {"key": "value"}
-
-; read first token
-; { -> start of object
-; [ -> start of array
-; _ -> start of identifier/simple value
-
 (defn read-char
   [input]
   (def int-value (.read input))
   (if (== -1 int-value)
     null-char
     (Character/valueOf (char int-value)))
-
-  )
-
-(defn append-char
-  [value-builder char-value]
-  (.append value-builder char-value)
-  ;  (prn (.toString value-builder))
   )
 
 
@@ -57,106 +38,6 @@
     )
   )
 
-(defn consume-token
-  [input depth state json-value value-builder]
-  (def next-char (read-char input))
-  (prn (str "json-value: " json-value ", in state: " state ", next-char: " next-char ", value-builder: " (.toString value-builder)))
-
-  (case state
-    "init" (if (.equals next-char object-start-token-char)
-             (consume-token input (+ 1 depth) key-start {} value-builder)
-
-             (if (.equals next-char array-start-token-char)
-               (consume-token input depth array-start [] value-builder)
-               (throw (RuntimeException. (str "invalid json: " (.toString next-char))))))
-    "array-start"
-    (if-not (.equals next-char array-end-token-char)
-      (do
-        (if-not (.equals next-char array-element-delimiter-char)
-          (do
-            (append-char value-builder next-char)
-            (consume-token input depth array-start json-value value-builder)
-            )
-
-          (do
-            (def array-element (typed-json-value (.toString value-builder)))
-            (.setLength value-builder 0)
-            (def updated-vector (concat json-value [array-element]))
-            (consume-token input depth array-start updated-vector value-builder)
-            updated-vector
-            )
-          )
-        )
-      (do
-        (def array-element (.toString value-builder))
-        (.setLength value-builder 0)
-        (def updated-vector (concat json-value [(typed-json-value array-element)]))
-        (prn "returning json array value: " updated-vector)
-        updated-vector
-        )
-
-      )
-    "key-start"
-    (if-not (.equals next-char identifier-delimiter-token-char)
-
-      ; keep building key
-      (do
-        ; ignore quote chars
-        (if-not (.equals next-char quote-char) (append-char value-builder next-char))
-
-        (consume-token input depth key-start json-value value-builder)
-        )
-
-      ; start building value
-      (do
-        (def identifier (.toString value-builder))
-        (.setLength value-builder 0)
-        ; todo - should pass new map?
-        (def latest-view (merge json-value {identifier
-                                            (consume-token input depth init json-value value-builder)}))
-        ;        (consume-token input depth key-start latest-view value-builder)
-
-        latest-view
-
-        )
-      )
-    "value-start"
-    (if (.equals next-char object-end-token-char)
-      (do
-        (def value (.toString value-builder))
-        (.setLength value-builder 0)
-        (typed-json-value value)
-
-        )
-      (if (.equals next-char array-element-delimiter-char)
-        (do
-          (prn "deal with end of value")
-          (def value (.toString value-builder))
-          (.setLength value-builder 0)
-          (def typed-val (typed-json-value value))
-          ;          (consume-token input depth key-start (merge json-value {}))
-          typed-val
-          )
-        (do
-          (append-char value-builder next-char)
-          (consume-token input depth value-start json-value value-builder)
-          ))
-
-      )
-
-    ))
-
-;
-
-;(prn (str "got me a token: " value-builder))
-
-(defn parse-json
-  [input-source handler-func]
-  (let [input (clojure.java.io/reader input-source)]
-    (def json-result (consume-token input 0 init nil (StringBuilder. "")))
-    (prn (str "output from parser: " json-result))
-    (handler-func json-result)
-    ))
 
 (defn match-char
   [expected actual]
@@ -186,29 +67,12 @@
     )
   )
 
-(defn debug-consume-scalar
-  [input accumulator]
-  (def value (consume-scalar input accumulator))
-  (prn (str "scalar: " value))
-
-  value
-  )
-
-(defn consume-end-of-object-token
-  [input]
-  (def next-char (read-char input))
-  (if (not (or (match-char next-char object-end-token-char) (match-char next-char null-char) (match-char next-char array-element-delimiter-char)))
-    (consume-end-of-object-token input)
-    )
-  )
+; TODO case statement for state
 
 (defn pj
   [input-source state accumulator]
   (def input (clojure.java.io/reader input-source))
   (def next-char (read-char input))
-
-  (prn (str "state: " state ", char: " next-char " - " accumulator))
-
   (if (= state nil)
     (if (match-char next-char array-start-token-char)
       (pj input "array" [])
@@ -216,7 +80,7 @@
         (pj input "object" {})
         (if (re-matches whitespace-pattern (.toString next-char))
           (pj input-source, state, accumulator)
-          (typed-json-value (debug-consume-scalar input (.toString next-char)))
+          (typed-json-value (consume-scalar input (.toString next-char)))
           )
 
         )
@@ -235,7 +99,7 @@
                 (concat accumulator [(pj input "object" {})] (pj input "array" []))
                 )
               (do
-                (concat accumulator [(typed-json-value (debug-consume-scalar input (.toString next-char)))] (pj input "array" [])))
+                (concat accumulator [(typed-json-value (consume-scalar input (.toString next-char)))] (pj input "array" [])))
               )
             )
           )
@@ -245,7 +109,7 @@
           accumulator
 
           (do
-            (def map-key (typed-json-value (debug-consume-scalar input (.toString next-char))))
+            (def map-key (typed-json-value (consume-scalar input (.toString next-char))))
             (def updated-map (merge accumulator {map-key (pj input nil nil)}))
             (pj input "object" updated-map)
             )
--- test/clj_ws/json_test.clj
+++ test/clj_ws/json_test.clj
@@ -4,51 +4,6 @@
   (:require [clojure.test :refer :all]
             [clj-ws.json :refer :all]))
 
-(defn validate-json-array
-  [json-result expected]
-  ;  (prn "expected: " expected (type expected) " as array " (into-array expected))
-  ;  (prn "actual: " json-result (type json-result) " as array " (into-array json-result))
-  ;  (prn "arrays equal? " (java.util.Arrays/equals (into-array json-result) (into-array expected)))
-  (is
-    (true? (java.util.Arrays/equals (into-array json-result) (into-array expected)))
-    )
-  )
-
-(defn validate-json-object
-  [json-result expected]
-  (println "json result key/vals")
-  (doseq [keyval json-result]
-    (prn (get keyval 0))
-    (prn (get keyval 1))
-    )
-  (println "expected key/vals")
-  (doseq [keyval expected]
-    (prn (get keyval 0))
-    (prn (get keyval 1))
-    (is
-      (and
-        (not (nil? (get json-result (get keyval 0))))
-        (is (true? (.equals (get json-result (get keyval 0)) (get expected (get keyval 0)))))
-        )
-      )
-    )
-  )
-
-(defn get-json-result-handler
-  [expected]
-  (fn [json-result]
-    (if (instance? clojure.lang.PersistentArrayMap json-result)
-      (validate-json-object json-result expected)
-      (validate-json-array json-result expected)
-      )
-
-    )
-  )
-
-(defn validate-json-parsing
-  [serialised-json expected-output]
-  (parse-json (new java.io.StringReader serialised-json) (get-json-result-handler expected-output))
-  )
 
 (deftest parse-single-element-integer-array
   (testing "single element integer array"
@@ -140,36 +95,3 @@
 
 
 
-
-; OLD TESTS
-
-;(deftest parse-simple-json-object
-;  (testing "Parse single json object"
-;    (validate-json-parsing "{\"1\":2}" {"1" 2})
-;    ))
-;
-;(deftest parse-simple-json-array
-;  (testing "Parse single json array"
-;    (validate-json-parsing "[1,2,3,4]" [1,2,3,4])
-;    ))
-
-;(deftest parse-complex-single-depth-json-object
-;  (testing "Parse complex single-depth json object"
-;    (validate-json-parsing "{\"foo\":1234,\"bar\":[1,23,4,6]" {"foo" "bar" "bar" [1, 23, 4, 6]})
-;    ))
-;
-;(deftest parse-complex-nested-json-object
-;  (testing "Parse complex nested json object"
-;    (validate-json-parsing "{\"foo\": 1234, \"bar\": {\"nest\": 1.23456}" {:foo "bar" :bar {:nest 1.23456}})
-;    ))
-;
-;(deftest whitespace
-;  (testing "Strip whitespace"
-;    (validate-json-parsing "{\"foo\"   :    1234  ,     \"bar\"    :    {    \"nest\"   :  1.23456   }"
-;      {:foo "bar" :bar {:nest 1.23456}})
-;    ))
-;
-;(deftest handle-unquoted-identifiers
-;  (testing "Parse single json object"
-;    (validate-json-parsing "{\"1\":2}" {:1 2})
-;    ))
